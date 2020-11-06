062a305d1daefcd1ad17e820ad9ee368890983c8
derekyee97 <dereky007@gmail.com>
2020-11-05 17:27:33 -0800

    split asteroids
    split asteroids

--- asteroid/Script Official/Asteroid/Asteroid .gd
+++ asteroid/Script Official/Asteroid/Asteroid .gd
@@ -1,12 +1,12 @@
 extends "res://Script Official/Weightless/Weightless.gd"
 
 #break down to smaller pieces so no large
-#var asteroidSmall = preload("res://Scenes/Asteroid Scene/AsteroidSmall .tscn")
-#var asteroidMedium = preload("res://Scenes/Asteroid Scene/AsteroidMedium.tscn")
+var asteroidSmall = load("res://Scenes/Asteroid Scene/AsteroidSmall .tscn")
+var asteroidMedium = load("res://Scenes/Asteroid Scene/AsteroidMedium.tscn")
 
-export(float) var explodeForce=300
+export(float) var explodeForce=50
 
-signal boom #explode
+signal explode
 
 #asteroid 3 sizes 
 enum Size {
@@ -16,28 +16,30 @@ enum Size {
 export(Size) var size= Size.LARGE
 var asteroidRadius
 
-func _ready():
-	#connect("boom",self,"_boom") #when boom sugnal received, call _boom
+func _ready(): #called whenver node added to scene
+	connect("explode",self,"_explode") #when explode sugnal received, call _explode
 	asteroidRadius=get_node("Sprite").texture.get_width()/2 * get_node("Sprite").scale
 	pass 
 	
-func _boom():
-	if size != Size.SMALL:
-		for i in range(0,3):
-			var asteroid
-			var offset_dir = PI * 2/3 * i
-			#match size:  #do dif things depending on size
-				#Size.MEDIUM:
-				#	asteroid=asteroidSmall.instance()
-				#Size.LARGE:
-				#	asteroid=asteroidMedium.instance()
-			#asteroid.position=position + asteroidRadius.rotated(offset_dir)
-		#	asteroid.linear_velocity=linear_velocity + Vector2(explode_dir, 0).rotated(offset_dir)
-		#	get_parent().add_child(asteroid)
-	#queue_free() #delete
+func _explode():
+	
+	if size!= Size.SMALL:
+		for i in range (0,3):
+			var offset= PI * 2 / 3 * i  
+			var asteroid 
+			match size: 
+				Size.MEDIUM:
+					asteroid=asteroidSmall.instance()
+				Size.LARGE:
+					asteroid=asteroidMedium.instance()
+			asteroid.position=position + asteroidRadius.rotated(offset) #need to set placement as previous asteroid! 
+			asteroid.linear_velocity=linear_velocity + Vector2(explodeForce,0).rotated(offset)
+			
+			get_parent().add_child(asteroid) #add to parent not self, dont want as subchild of another asteroid
+	
+	queue_free()
+	sleeping=true
 	pass
-
-
 
 #only use when physics is NOT applied 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
--- asteroid/Script Official/Missile/Missile.gd
+++ asteroid/Script Official/Missile/Missile.gd
@@ -1,4 +1,4 @@
-extends Node
+extends RigidBody2D
 
 
 # Declare member variables here. Examples:
@@ -8,9 +8,18 @@ extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	set_max_contacts_reported(1)
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+#seeing if missile hit something
+func _integrate_forces(state):
+	var contacts=state.get_contact_count()
+	for i in range(contacts):
+		var contact = state.get_contact_collider_object(i)
+		if contact.get_script().has_script_signal("explode"):
+			contact.emit_signal("explode")
+			queue_free() #delete missile
+			sleeping=true
+	pass
+	
